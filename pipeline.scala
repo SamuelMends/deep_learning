6832d1ba21adf6686dbb9310559302dc1906a16d
SamuelMends <samuelqmendes@gmail.com>
2024-10-02 23:14:53 -0300

    Update pipeline.scala

---
+++ pipeline.scala
@@ -0,0 +1,6 @@
+// Projeto de Engenharia de Dados com Linguagem Scala
+// Processo de ETL em banco de Dados Relacional
+
+//Imports
+import com.github.totoshi.csv
+import java.sql.{Connection, DriverManager, ResultSet}
